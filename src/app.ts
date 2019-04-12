4bd5134cd9a6dc620ebfd6ef425e703a43319fd9
n3op2 <paulius.michelevicius@gmail.com>
2019-04-12 21:12:21 +0100

    Playing around, building model.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,34 @@
 import express from 'express';
 import * as http from 'http';
 import DB from './lib/db';
-import { Pair } from './models/Pair';
-
-console.log(Pair.create());
+import Pair from './models/Pair';
 
 const db = new DB('mongodb://127.0.0.1/trader').connect();
 const app = express();
+const EU = new Pair('EURUSD');
+
+console.log(EU);
+
+const gatherData = (t: number) => {
+  setInterval(async () => {
+    const s: number = new Date().getTime();
+    await EU.test(res => {
+      console.log(`interval [${t}]`, res)
+    });
+    const e: number = new Date().getTime();
+    console.log(s - e);
+  }, t);
+};
+
+gatherData(3000);
+gatherData(1000);
+
+
+
+EU.test(res => {
+  const data = JSON.parse(res);
+  console.log(data.rates.EURUSD);
+});
 
 app.get('/', (req, res) => {
   console.log('app.get(/)');
--- src/models/Pair.ts
+++ src/models/Pair.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
 import PairSchema from '../schemas/Pair';
+import uuidv1 from 'uuid';
+import axios from 'axios';
 
-const pair = mongoose.model('Pair', PairSchema); 
+const pair = mongoose.model('pairs', PairSchema); 
+const url: string = 'https://www.freeforexapi.com/api/live';
 
+/*
 export const Pair = {
   create: function() {
     const rate = new pair({
@@ -15,4 +19,50 @@ export const Pair = {
     });
   }
 };
+*/
 
+export interface IPair {
+  uuid: string;
+};
+
+const GetData: (url: string, pair: string) => Promise<string> =
+  function(url: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      axios.get(url).then((res) => {
+        return resolve(JSON.stringify(res.data));
+      }).catch((err) => {
+        return reject(err);
+      });;
+    });
+  };
+
+export default class Pair implements IPair {
+  private getUrl: string;
+  private name: string;
+  uuid = uuidv1();
+
+  constructor(name: string) {
+    this.name = name;
+    this.getUrl = `${url}?pairs=${name}`;
+  };
+
+  public test = async (cb: (res: string) => void) => {
+    await GetData(this.getUrl, this.name).then(pair => {
+      return cb(pair);
+    }).catch((err) => console.log(err));
+  };
+
+  public create = async (rate: number, fhn: string, cb: (res: boolean) => void) => {
+    const _pair = new pair({
+      uuid: this.uuid 
+    });
+    await _pair.save((saveErr, saveRes) => {
+      if (saveErr) {
+        console.log('Error: ', saveErr);
+        return cb(false);
+      }
+      console.log('Info: ', saveRes);
+      cb(true);
+    });
+  };
+};
